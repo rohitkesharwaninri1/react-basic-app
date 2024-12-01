84c3ae5021cce41975651402a2dbe68beec70e7d
Rohit <rohit.kesharwani@timesgroup.com>
2024-12-01 16:22:10 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import Counter from './components/Counter';
-import User from './components/User';
-import GetComponent from './components/API_generic/GetComponent'
+// import Counter from './components/Counter';
+
 
 export default function App() {
   return (
     <div>
       Hello
-      <Counter />
-      <User/>
-      {/* API */}
-      <GetComponent/>
+     
     </div>
   )
 }
