af66fce71a983cf00365d0ca931a28b913baffe5
Rohit <rohitkesharwaninri1@gmail.com>
2024-12-08 20:21:35 +0530

    Added Chess board and initial css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import Counter from './components/CustomHook/Counter';
-import A from './components/HOC/A';
-import B from './components/HOC/B';
-import C from './components/HOC/C';
-import HOC from './components/HOC/HOC';
-import SimpleReduxCounter from  './components/SimpleReduxCounter/SimpleReduxCounter';
-// const EnhancedA = HOC(A);
-
+import "./App.css";
+import React, { useState } from "react";
 export default function App() {
+  const [grid, setGrid] = useState(5);
+  const data = Array.from({ length: grid * grid }, (_, i) => i);
   return (
-    <div>
+    <div className="App">
+      <h4>Enter Grid Size</h4>
 
-      {/* Hooks */}
-      {/* <Counter/> */}
-
-
-      {/* HOC */}
-      {/* <EnhancedA lastName="kesharwani"/>
-      <B lastName="Gupta"/>
-      <C/> */}
-
-      {/* SimpleReduxCounter */}
-      <SimpleReduxCounter/>
-
+      <input
+        value={grid}
+        type="number"
+        onChange={(e) => setGrid(e.target.value)}
+      />
+      {grid && (
+        <div
+          className="grid-container"
+          style={{ gridTemplateColumns: `repeat(${grid}, 1fr)` }}
+        >
+          {data.map((i) => {
+            const row = Math.floor(i / grid);
+            const col = i % grid;
+            const backgroundColor =
+              (row + col) % 2 === 0 ? "#434342" : "#E6E6E6";
+            return (
+              <div
+                className="grid-item"
+                style={{
+                  backgroundColor: backgroundColor,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      )}
     </div>
   )
 }
