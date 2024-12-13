e45a775a7819b45230b0139d084ba848084d9b1a
Rohit <rohitkesharwaninri1@gmail.com>
2024-12-13 22:07:42 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react'
-import Counter from './components/CustomHook/Counter';
-import A from './components/HOC/A';
-import B from './components/HOC/B';
-import C from './components/HOC/C';
-import HOC from './components/HOC/HOC';
-import SimpleReduxCounter from  './components/SimpleReduxCounter/SimpleReduxCounter';
-// const EnhancedA = HOC(A);
 
-export default function App() {
+function App() {
   return (
-    <div>
-
-      {/* Hooks */}
-      {/* <Counter/> */}
-
-
-      {/* HOC */}
-      {/* <EnhancedA lastName="kesharwani"/>
-      <B lastName="Gupta"/>
-      <C/> */}
-
-      {/* SimpleReduxCounter */}
-      <SimpleReduxCounter/>
-
-    </div>
+    <div>App</div>
   )
 }
+
+export default App
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import {reducer} from "./reducer";
-
-
-// Create Redux store
-const store = createStore(reducer);
-
-export default store;
